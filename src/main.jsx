4bc68a6336ea32ef3690d20a807aefd879bdc547
pedroantuness <RM551409@fiap.com.br>
2023-09-01 11:43:50 -0300

    refatoramento da lista de produtos mais imgs

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './routes/Home.jsx'
-import Produtos from './routes/Produtos.jsx'
-import EditarProdutos from './routes/EditarProdutos.jsx'
-import Error from './routes/Error.jsx'
-import ExcluirProdutos from './routes/ExcluirProdutos.jsx'
+//Bloco de criação das rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Error from './routes/Error.jsx';
+import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-        { path: '/', element: <Home /> },
-        { path: '/produtos', element: <Produtos /> },
-        { path: '/editar/produtos/:id', element: <EditarProdutos /> },
-        { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> }
+const router = createBrowserRouter(
+  [
+    {path: '/',  element : <App />,
+    errorElement: <Error/>,
+    children:[
+        { path: '/', element : <Home/>},
+        { path: '/produtos', element : <Produtos/>},
+        { path: '/editar/produto/:id', element : <EditarProdutos/>},
+        { path: '/excluir/produto/:id', element : <ExcluirProdutos/>},
       ]
-    }
-  ])
+  }
+ ]
+)
 
-// Bloco de criação das rotas
+//Bloco de criação das rotas
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,37 +3,42 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {AiOutlineEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import "./Produtos.css";
 
-export default function Produtos(){
-    
-    return(
-        <>
-            <div>
-                <h1>Produto</h1>
+export default function Produtos() {
 
-                <table className="tabelaProd">
-                    <tr>
-                        <th>ID</th>
-                        <th>NOME</th>
-                        <th>PREÇO</th>
-                        <th><Editar /> / <Excluir /></th>
-                    </tr>
+    const estiloImg = {
+        width: "100px",
+        height: "100px",
+    }
 
-                    {ListaProdutos.map((produto, indice) => (
+  return (
+    <>
+        <div>
+            <h1>PRODUTOS</h1>
+
+            <table className="tabelaProd">
+                <tr>
+                    <th>ID</th>
+                    <th>NOME</th>
+                    <th>DESCRIÇÃO</th>
+                    <th>PREÇO</th>
+                    <th>IMG</th>
+                    <th><Editar/> / <Excluir/></th>
+
+                </tr>
+            
+                {ListaProdutos.map( (produto, indice)=>(
                         <tr key={indice}>
                             <td>{produto.id}</td>
                             <td>{produto.nome}</td>
+                            <td>{produto.desc}</td>
                             <td>{produto.preco}</td>
-                            <td>
-                                <Link to={`/editar/produto/${produto.id}`}><Editar /></Link> / 
-                                <Link to={`/excluir/produto/${produto.id}`}> <Excluir /></Link>
-                            </td>
+                            <td><img style={estiloImg} src={`${produto.img}`} alt={`${produto.desc}`}/></td>
+                            <td> <Link to={`/editar/produto/${produto.id}`}><Editar/></Link> / <Link to={`/excluir/produto/${produto.id}`}><Excluir/></Link> </td>
                         </tr>
-                    ))}
+                ))}
+            </table>
 
-                </table>
-
-            </div>
-        </>
-    )
+        </div>
+    </>
+  )
 }
-
