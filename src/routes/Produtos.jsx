fbf59031ee3c9caba463f43ea6ccd6e2dbed0337
pedroantuness <rm551409@fiap.com.br>
2023-09-13 09:12:56 -0300

    Apresentação ao css module.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,41 +1,33 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import { AiOutlineEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
-import "./Produtos.css";
+import classes from "./Produtos.module.css";
 
 export default function Produtos() {
-
-    const estiloCelulas = {
-        widht: '10%',
-        textAlign:"center",
-        fontWeight:"bold"
-    };
-
-
 
     return(
         <>
             <div>
                 <h1>Produtos</h1>
 
-                <table className="tabelaProd">
+                <table className={classes.tabelaProd}>
                     <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Descrição</th>
-                        <th>Preço</th>
-                        <th>IMG</th>
-                        <th><Editar/> / <Excluir/></th>
+                        <th className={classes.tabelaCabecalho}>ID</th>
+                        <th className={classes.tabelaCabecalho}>Nome</th>
+                        <th className={classes.tabelaCabecalho}>Descrição</th>
+                        <th className={classes.tabelaCabecalho}>Preço</th>
+                        <th className={classes.tabelaCabecalho}>IMG</th>
+                        <th className={classes.tabelaCabecalho}><Editar/> / <Excluir/></th>
                     </tr>
 
                     {ListaProdutos.map( (produto, indice) => (
-                        <tr className={(produto.id %2==0) ? "linhaCinza" : "linhaBranca"} key={indice}>
-                            <td style={estiloCelulas}>{produto.id}</td>
-                            <td>{produto.nome}</td>
-                            <td>{produto.desc}</td>
-                            <td>{produto.preco}</td>
-                            <td><img style={{width:"100px"}} src={`${produto.img}`} alt={`${produto.desc}`}/></td>
-                            <td><Link to={`/editar/produto/${produto.id}`}><Editar/></Link> / 
+                        <tr className={classes.tabelaLinha} key={indice}>
+                            <td className={classes.tabelaDados}>{produto.id}</td>
+                            <td className={classes.tabelaDados}>{produto.nome}</td>
+                            <td className={classes.tabelaDados}>{produto.desc}</td>
+                            <td className={classes.tabelaDados}>{produto.preco}</td>
+                            <td className={classes.tabelaDados}><img className={classes.tblImg} src={`${produto.img}`} alt={`${produto.desc}`}/></td>
+                            <td className={classes.tabelaDados}><Link to={`/editar/produto/${produto.id}`}><Editar/></Link> / 
                             <Link to={`/excluir/produto/${produto.id}`}><Excluir/></Link></td>
                         </tr>
                     ))}
