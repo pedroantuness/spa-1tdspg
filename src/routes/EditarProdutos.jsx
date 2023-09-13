e94a244673c9b92e3d0e3a87ee02ee190d12bd3f
pedroantuness <rm551409@fiap.com.br>
2023-09-13 08:45:20 -0300

    Editar realizado com useState/useNavigate, destructuring

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function EditarProdutos() {
     
     document.title = "Editar Produtos";
+
+    const navigate = useNavigate();
 
     //Recuperando o id do produto com o HOOK useParam();
     const {id} = useParams();
@@ -22,33 +24,61 @@ export default function EditarProdutos() {
 
         
 
-        const handlerEditarProduto = (event) => {
-            event.preventDefault();
+        const handleChange = (event) => {
+            // Destructuring
+            const{name, value} = event.target;
+            // Inserir os dados no objeto produto atraves do setProduto(...)
+            setProduto({...produto, [name] : value});
         }
+
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+
+            //indice que será utilizado para a sobreposição do produto na lista
+            let indice;
+
+            //Localização do indice na lista
+            ListaProdutos.forEach((item, index) => {
+                if(item.id == produto.id){
+                    indice = index
+                }            
+            });
+
+            // Utilizando o metodo splice para alterar o produto no indice especificado
+            ListaProdutos.splice(indice, 1, produto);
+            alert("Produto Alterado!");
+
+            navigate("/produtos");
+
+        }
+
 
     return(
         <>
             <div>
 
+
+
                 <h1>EDITAR-PRODUTOS</h1>
-                <form action="#" method="get">
+                <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>EDITAR PRODUTO</legend>
                     <div>
                         <label htmlFor="idProduto">Nome Produto:</label>
-                        <input type="text" name="nomeProduto" id="idProduto" defaultValue={produto.nome} onChange={handlerEditarProduto} />
+                        <input type="text" name="nome" id="idProduto" value={produto.nome} onChange={handleChange} />
                     </div>
                     <div>
                         <label htmlFor="idProduto">Preço produto:</label>
-                        <input type="text" name="precoProduto" id="idProduto" defaultValue={produto.preco} onChange={handlerEditarProduto} />
+                        <input type="text" name="preco" id="idProduto" value={produto.preco} onChange={handleChange} />
                     </div>
                     <div>
                         <label htmlFor="idProduto">Descrição produto:</label>
-                        <input type="text" name="descProduto" id="idProduto" defaultValue={produto.desc} onChange={handlerEditarProduto} />
+                        <input type="text" name="desc" id="idProduto" value={produto.desc} onChange={handleChange} />
                     </div>
                     <div>
                         <label htmlFor="idProduto">Imagem produto:</label>
-                        <input type="text" name="imgProduto" id="idProduto" defaultValue={produto.img} onChange={handlerEditarProduto} />
+                        <input type="text" name="img" id="idProduto" value={produto.img} onChange={handleChange} />
                     </div>
                     <div>
                         <button >EDITAR</button>
