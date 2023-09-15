b4241740cee330a18536489bbb8be26b9a760c64
pedroantuness <RM551409@fiap.com.br>
2023-09-15 11:12:32 -0300

    Exemplo useEffect x 3.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -7,10 +7,22 @@ import { useEffect, useState } from "react";
 export default function Produtos() {
 
     const [counter, setCounter] = useState(0);
+    const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
+    const [counter2, setCounter2] = useState(0);
 
     useEffect(() => {
         console.log("useEffect que renderiza sempre!")
-    })
+    });
+
+    useEffect(() => {
+        setNovaListaProdutos(ListaProdutos);
+        console.log("UseEffect que renderiza apenas uma vez!")
+    }, [ ]);
+
+    useEffect(() => {
+        console.log("UseEffect que renderiza apenas se o objeto/elemento/constante que esta sendo monitorado no array de dependencias sofrer atualização!")
+    }, [counter2]);
+    
 
     return(
         <>
@@ -19,6 +31,9 @@ export default function Produtos() {
 
                 <div>
                     <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
+                </div>
+                <div>
+                    <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
                 </div>
 
                 <table className={classes.tabelaProd}>
@@ -34,7 +49,7 @@ export default function Produtos() {
                         </tr>
                     </thead>
                     <tbody>
-                        {ListaProdutos.map( (produto, indice) => (
+                        {novaListaProdutos.map( (produto, indice) => (
                             <tr className={classes.tabelaLinha} key={indice}>
                                 <td className={classes.tabelaDados}>{produto.id}</td>
                                 <td className={classes.tabelaDados}>{produto.nome}</td>
@@ -46,9 +61,9 @@ export default function Produtos() {
                             </tr>
                         ))}
                     </tbody>
-                    <tfoot>
+                    {/* <tfoot>
                         <td colSpan={6}>PRODUTOS</td>
-                    </tfoot>
+                    </tfoot> */}
                 </table>
 
             </div>
