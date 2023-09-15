c4f3f18e79b325a31202449160bbaf8f5f81c33c
pedroantuness <RM551409@fiap.com.br>
2023-09-15 11:53:16 -0300

    adicionado css ao app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho";
 import Rodape from "./components/Rodape";
+import style from "./App.module.css"
 
 export default function App() {
-
   //area declarativa
   return (
     // area imperativa
     <>
-      <Cabecalho/>
-      <Outlet/>
-      <Rodape/>
+      <div className={style.container}>
+        <Cabecalho />
+        <Outlet />
+        <Rodape />
+      </div>
     </>
-  )
+  );
 }
