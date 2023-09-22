bec3832b4ac0ef1132213a0d3a7721d1e339ff6a
pedroantuness <RM551409@fiap.com.br>
2023-09-22 10:30:14 -0300

    Refatorando o componente Produtos.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -6,36 +6,17 @@ import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
-    const [counter, setCounter] = useState(0);
     const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
-    const [counter2, setCounter2] = useState(0);
-
-    useEffect(() => {
-        console.log("useEffect que renderiza sempre!")
-    });
 
     useEffect(() => {
         setNovaListaProdutos(ListaProdutos);
         console.log("UseEffect que renderiza apenas uma vez!")
     }, [ ]);
 
-    useEffect(() => {
-        console.log("UseEffect que renderiza apenas se o objeto/elemento/constante que esta sendo monitorado no array de dependencias sofrer atualização!")
-    }, [counter2]);
-    
-
     return(
         <>
             <div>
                 <h1>Produtos</h1>
-
-                <div>
-                    <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
-                </div>
-                <div>
-                    <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-                </div>
-
                 <table className={classes.tabelaProd}>
 
                     <thead className={classes.tabelaCabecalho}>
