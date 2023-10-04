319f6e20afb5ff0966c24a0365285d35249948e4
pedroantuness <pedro280702@gmail.com>
2023-10-04 09:23:50 -0300

    Criando logica para determinar em que pagina ativa com sass.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Cabecalho.scss"
 
 export default function Cabecalho(){
+
+    const rotaAtual = useLocation();
 
     return(
         <>
             <header className="cabecalho">
                 <ul>
-                    <li><Link to="/">HOME</Link></li>
-                    <li><Link to="/produtos">Produtos</Link></li>
+                    <li><Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>HOME</Link></li>
+                    <li><Link to="/produtos" className={rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
                 </ul>
             </header>
 
